7104b7589e10b7ec8c5bdaef35dfce789da053b7
Gilbert <gilbertwong96@icloud.com>
2026-07-17 20:48:54 +0800

    docs: fix date in obscurax binding design spec

---
+++ lib/obscurax.ex
@@ -0,0 +1,18 @@
+defmodule Obscurax do
+  @moduledoc """
+  Documentation for `Obscurax`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Obscurax.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/obscurax_test.exs
@@ -0,0 +1,8 @@
+defmodule ObscuraxTest do
+  use ExUnit.Case
+  doctest Obscurax
+
+  test "greets the world" do
+    assert Obscurax.hello() == :world
+  end
+end
